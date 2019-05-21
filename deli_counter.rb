58dbde38ba53846cb8740e868fd1e7106e29dba0
Erika Davis <davis2erika@gmail.com>
2019-05-21 12:14:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
+  if katz_deli.empty? == true
   puts "There is nobody waiting to be served!"
+  else
+   puts "Currently serving {katz_deli.first}"
+end
+katz_deli.shift
 end
